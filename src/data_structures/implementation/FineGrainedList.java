ae77ee0e3dc7f464f7b3750c13dc4f3ee76655f9
Rink <rinkstiekema@hotmail.com>
2016-12-08 16:52:09 +0100

    Added fine grained list

--- src/data_structures/implementation/FineGrainedList.java
+++ src/data_structures/implementation/FineGrainedList.java
@@ -1,20 +1,98 @@
 package data_structures.implementation;
 
 import java.util.ArrayList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import data_structures.Sorted;
 
 public class FineGrainedList<T extends Comparable<T>> implements Sorted<T> {
+    private FineNode<T> head;
+
+    public FineGrainedList() {
+        head = new FineNode<T>(null);
+    }
 
     public void add(T t) {
-        throw new UnsupportedOperationException();
+        head.lock();
+        FineNode<T> predecessor = head;
+
+        if(head.getNext() == null) {
+            head.setNext(new FineNode<T>(t));
+            head.unlock();
+            return;
+        }
+
+        try {
+            FineNode<T> current = predecessor.getNext();
+            current.lock();
+
+            try {
+                while(current.getData().compareTo(t) < 0){
+                    if(current.getNext() == null){
+                        current.setNext(new FineNode<T>(t));
+                        return;
+                    }
+                    predecessor.unlock();
+                    predecessor = current;
+                    current = current.getNext();
+                    current.lock();
+                }
+
+                FineNode<T> newNode = new FineNode<T>(t);
+                newNode.setNext(current);
+                predecessor.setNext(newNode);
+            } finally {
+                current.unlock();
+            }
+        } finally {
+            predecessor.unlock();
+        }
     }
 
     public void remove(T t) {
-        throw new UnsupportedOperationException();
+        FineNode<T> predecessor = null,
+                    current = null;
+
+        head.lock();
+        try {
+            predecessor = head;
+            if(predecessor.getNext() == null){
+                head.unlock();
+                return;
+            }
+
+            current = predecessor.getNext();
+            current.lock();
+
+            try {
+                while(current.getData().compareTo(t) < 0){
+                    predecessor.unlock();
+                    predecessor = current;
+                    current = current.getNext();
+                    current.lock();
+                }
+
+                if(current.getData().compareTo(t) == 0){
+                    predecessor.setNext(current.getNext());
+                }
+            } finally {
+                current.unlock();
+            }
+        } finally {
+            predecessor.unlock();
+        }
     }
 
     public ArrayList<T> toArrayList() {
-        throw new UnsupportedOperationException();
+        FineNode<T> current = head.getNext();
+        ArrayList<T> list = new ArrayList<>();
+
+        while(current != null){
+            list.add((T) current.getData());
+            current = current.getNext();
+        }
+
+        return list;
     }
 }
