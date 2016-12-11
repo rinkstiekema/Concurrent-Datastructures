250c2a9da64300761660e325a0056f2e64848239
Tim Nederveen <tim.nederveen@hotmail.com>
2016-12-11 19:57:43 +0100

    Improvements to testing script

--- testScript.sh
+++ testScript.sh
@@ -2,10 +2,10 @@
 
 iterateThreads () {
 	echo "DATASTRUCTURE $1    WORKTIME $2";
-	./bin/test_data_structures $1 1 2000 $2 &&
-	./bin/test_data_structures $1 10 2000 $2 &&
-	./bin/test_data_structures $1 100 2000 $2 &&
-	./bin/test_data_structures $1 1000 2000 $2
+	./bin/test_data_structures $1 1 2000 $2 | grep time &&
+	./bin/test_data_structures $1 10 2000 $2 | grep time &&
+	./bin/test_data_structures $1 100 2000 $2 | grep time &&
+	./bin/test_data_structures $1 1000 2000 $2 | grep time
 	echo;
 }
 
@@ -27,16 +27,3 @@ iterateWorkTimeAndThreads cgt;
 iterateWorkTimeAndThreads fgl;
 # iterateWorkTimeAndThreads fgt;
 }
-
-
-
-# echo &&
-# ./bin/test_data_structures cgl 1 20000 2 &&
-# ./bin/test_data_structures cgl 10 20000 2 &&
-# ./bin/test_data_structures cgl 100 20000 2 &&
-# ./bin/test_data_structures cgl 1000 20000 2 &&
-# echo &&
-# ./bin/test_data_structures cgl 1 20000 5 &&
-# ./bin/test_data_structures cgl 10 20000 5 &&
-# ./bin/test_data_structures cgl 100 20000 5 &&
-# ./bin/test_data_structures cgl 1000 20000 5 &&
